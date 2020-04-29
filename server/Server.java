11bec4a577ca31072f2065709a8e2e06ac227cae
Ganeshwara Hananda <ganeshwara@grakn.ai>
2020-04-29 20:53:18 +0100

    compile time dependency done

--- server/Server.java
+++ server/Server.java
@@ -2,10 +2,27 @@ package server;
 
 import play.Application;
 import play.ApplicationLoader;
+import play.BuiltInComponentsFromContext;
+import play.filters.components.HttpFiltersComponents;
+import play.routing.Router;
+import router.Routes;
 
 public class Server implements ApplicationLoader {
+    
     @Override
     public Application load(Context context) {
         return new Component(context).application();
+    }
+
+    static class Component extends BuiltInComponentsFromContext implements HttpFiltersComponents {
+        public Component(ApplicationLoader.Context context) {
+            super(context);
+        }
+
+        @Override
+        public Router router() {
+            event.github.Routes eventGithub = new event.github.Routes();
+            return new Routes(scalaHttpErrorHandler(), eventGithub).asJava();
+        }
     }
 }
