582b4c983af8f48a0b19221fb46134f07db38488
Ganeshwara Hananda <ganeshwara@grakn.ai>
2020-04-30 15:54:01 +0100

    minor refactor

--- server/Server.java
+++ server/Server.java
@@ -12,7 +12,6 @@ import play.routing.Router;
 import router.Routes;
 
 public class Server implements ApplicationLoader {
-
     @Override
     public Application load(Context context) {
         return new Component(context).application();
@@ -21,14 +20,15 @@ public class Server implements ApplicationLoader {
     static class Component extends BuiltInComponentsFromContext implements HttpFiltersComponents {
         private ActorSystem<Object> actorSystem = ActorSystem.create(Behaviors.empty(), "server");
         private ActorMaterializer actorMaterializer = ActorMaterializer.create(Adapter.toClassic(actorSystem));
+        private event.github.Router eventGithub = new event.github.Router();
+        private service.Router service = new service.Router(actorSystem, actorMaterializer);
+
         public Component(ApplicationLoader.Context context) {
             super(context);
         }
 
         @Override
         public Router router() {
-            event.github.Router eventGithub = new event.github.Router();
-            service.Router service = new service.Router(actorSystem, actorMaterializer);
             return new Routes(scalaHttpErrorHandler(), eventGithub, service).asJava();
         }
     }
