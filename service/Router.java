9886c8ebff127e81ed20e36e0de67d0a004b60e3
Ganeshwara Hananda <ganeshwara@grakn.ai>
2020-05-01 12:07:00 +0100

    finish getting websocket to work

--- service/Router.java
+++ service/Router.java
@@ -6,18 +6,12 @@ import play.mvc.Controller;
 import play.mvc.Result;
 import play.mvc.WebSocket;
 
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 
 public class Router extends Controller {
     private SessionRegistry sessionRegistry = new SessionRegistry();
 
     public WebSocket index() {
-        CompletionStage<F.Either<Result, Flow<String, String, ?>>> flow =
-            CompletableFuture.supplyAsync(() -> F.Either.Right(Flow.fromFunction(msg -> {
-                System.out.println("index::out -> ... '" + msg + "'");
-                return msg;
-            })));
         CompletionStage<F.Either<Result, Flow<String, String, ?>>> actor = sessionRegistry.newSession();
         return WebSocket.Text.acceptOrResult(request -> actor);
     }
